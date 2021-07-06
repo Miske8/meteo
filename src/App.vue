1cc281318845f9c1cb340fda8642f3e765c9b82d
vhash <vanhaaren.lucas+dev@gmail.com>
2021-07-06 10:41:40 +0200

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MeteoDetails />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MeteoDetails from './components/MeteoDetails.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MeteoDetails
   }
 }
 </script>
---
+++ src/components/MeteoDetails.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="meteo-wrapper">
+    <!-- CODE ICI -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MeteoDetails'
+}
+</script>
+
+<style scoped>
+.meteo-wrapper {
+  background-color: grey;
+}
+</style>
